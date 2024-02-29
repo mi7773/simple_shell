6f14bad416f266fbf369283dcddcef31b4a7df63
ez-114 <morganezz226@gmail.com>
2024-02-29 23:15:14 +0200

    fix: task 4 checks. with memallocation management

--- nactive.c
+++ nactive.c
@@ -6,48 +6,65 @@
 #include "main.h"
 
 /**
+ * cmd_lines - command lines with newline
+ * @lineptr: read line
+ * Return: command lines in array
+*/
+char **cmd_lines(char *lineptr)
+{
+	int lines = 0;
+	char **ca = 0;
+
+	lines = cdel(lineptr, '\n');
+	ca = (char **) malloc(sizeof(char *) * (lines + 2));
+	if (ca == 0)
+	{
+		return (0);
+	}
+	ca = mystok(lineptr, "\n", ca);
+	return (ca);
+}
+
+/**
  * nactive - draft
  * @av0: draft
  * Return: draft
  */
 int nactive(char *av0)
 {
-	int i = 1, r = 0;
+	int i = 0, r = 0;
 	(void) av0;
+	char *lineptr = 0, **ca = 0, **pa = 0, **cla = 0;
 
-	do {
-		char *lineptr = 0, **ca = 0, **pa = 0;
+	lineptr = myread();
+	cla = cmd_lines(lineptr);
+	while (cla[i] != NULL)
+	{
+		ca = coarr(cla[i]);
+		pa = patharr(ca[i]);
 
-		lineptr = mygetl();
-		if (lineptr == 0)
-		{
-			break;
-		}
-		ca = coarr(lineptr);
-		pa = patharr(ca[0]);
 		if (mystat(ca, pa) == 3)
-		{
 			penv();
-		}
 		else if (mystat(ca, pa) == 0)
-		{
 			myexe(ca, av0);
-		}
 		else if (mystat(ca, pa) == 2)
 		{
+			free(cla);
 			free(lineptr);
 			myfree(ca, pa);
-			break;
+			return (2);
 		}
 		else if (ca[0] != 0)
 		{
 			perror(av0);
 			r = 127;
 		}
-		free(lineptr);
-		myfree(ca, pa);
 		i++;
-	} while (1);
+		myfree(ca, pa);
+	}
+	free(cla);
+	free(lineptr);
+	myfree(ca, pa);
 
 	return (r);
 }
