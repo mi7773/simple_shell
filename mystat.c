1debd569a349e07e22137da071e82c97611232b1
Mahmoud Ismail <ismailm7373@gmail.com>
2024-03-01 20:26:45 +0000

    task

--- mystat.c
+++ mystat.c
@@ -1,4 +1,5 @@
 #include <sys/stat.h>
+#include <unistd.h>
 #include "main.h"
 
 /**
@@ -10,39 +11,43 @@
 int mystat(char **ca, char **pa)
 {
 	struct stat st;
+	size_t size = 0;
+	char *cwdr = 0;
 
+	cwdr = getcwd(cwdr, size), chdir("/");
 	if (ca[0] == 0)
 	{
+		chdir(cwdr);
 		return (1);
 	}
 	if (myen(ca[0]) == 3)
 	{
+		chdir(cwdr);
 		return (3);
 	}
 	else if (myexit(ca[0]) == 2)
 	{
+		chdir(cwdr);
 		return (2);
 	}
 	else if (stat(ca[0], &st) == 0)
 	{
+		chdir(cwdr);
 		return (0);
 	}
 	else
 	{
-		int i;
+		int i = 0;
 
-		i = 0;
 		while (pa[i])
 		{
 			if (stat(pa[i], &st) == 0)
 			{
-				ca[0] = pa[i];
-
+				chdir(cwdr), ca[0] = pa[i];
 				return (0);
 			}
 			i++;
 		}
-	}
-
+	} chdir(cwdr);
 	return (1);
 }
